9e821d41d2d4311df034b7ba90dffa54433fba68
Diego Escobar <diegofe.escobar@umariana.edu.co>
2024-03-04 22:53:24 -0500

    Version 1

---
+++ src/expocanina/NombreDuplicadoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package expocanina;
+
+/**
+ *
+ * @author Diego Escobar y Deiner Coral
+ */
+public class NombreDuplicadoException extends Exception {
+
+    public NombreDuplicadoException() {
+    }
+    
+}
